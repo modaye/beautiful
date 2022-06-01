83794d8a0921a8087130e82b3ce8cbe6eee85ba7
阿达达 <1174501146@qq.com>
2022-06-01 17:27:06 +0800

    init

--- src/tests/test_client.py
+++ src/tests/test_client.py
@@ -1,6 +1,9 @@
+import pytest
+from requests.exceptions import ConnectTimeout
 from beautiful.client import BeautifulClient
 
 
 def test_client():
     client = BeautifulClient()
-    assert client.card_coord(card_ids=[1]) == []
+    with pytest.raises(ConnectTimeout):
+        client.card_coord([1, 2, 3])
